82266c0e780832869d9a70e0fcf8726a1f7a3ded
Isaias Medina <isamed92@gmail.com>
2024-01-30 21:05:20 -0300

    feat: initial commit

---
+++ src/ReactGPT.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ReactGPT = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>ReactGPT</h1>
+      <p>ReactGPT is a React component for Google Publisher Tag.</p>
+      <p>It is a work in progress.</p>
+    </div>
+  );
+};
