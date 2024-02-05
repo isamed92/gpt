0785e27fe2c35d53b4334e1526f69ceaca13be4d
Isaias Medina <isamed92@gmail.com>
2024-02-05 01:48:24 -0300

    Add pros-cons-stream.use-case and update ProsConsStreamPage

--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,5 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from '../../components';
+import { prosConsStreamUseCase } from '../../../core/use-cases';
+
+interface Message {
+  text: string;
+  isMyMessage: boolean;
+}
 
 export const ProsConsStreamPage = () => {
-  return <div>ProsConsStreamPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (message: string) => {
+    setIsLoading(true);
+    setMessages((messages) => [
+      ...messages,
+      { text: message, isMyMessage: true },
+    ]);
+
+    await prosConsStreamUseCase(message);
+
+    setIsLoading(false);
+
+    // Añadir el mensaje de respuesta
+  };
+
+  return (
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          <GptMessage text='Que deseas comparar hoy?' />
+          {messages.map((message) => (
+            <div
+              key={message.isMyMessage + message.text}
+              className='col-span-12'
+            >
+              {message.isMyMessage ? (
+                <MyMessage text={message.text} />
+              ) : (
+                <GptMessage text={message.text} />
+              )}
+            </div>
+          ))}
+
+          {isLoading ? (
+            <div className='col-start-1 col-end-12 fade-in'>
+              <TypingLoader />
+            </div>
+          ) : null}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder='Escribe tu mensaje...'
+        disableCorrections
+      />
+    </div>
+  );
 };
